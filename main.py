696b1edf1e0882cbe80d6c77b4a4aca47949947b
yatabis <39yatabis.rim8820xxx@gmail.com>
2019-06-12 10:49:26 +0900

    add: first question

--- main.py
+++ main.py
@@ -1,6 +1,46 @@
 from bottle import route, request, run
+import json
 import os
+import psycopg2
+from psycopg2.extras import DictCursor
+import random
 import requests
+
+# CONST
+DSN = os.environ.get('DATABASE_URL')
+CAT = os.environ.get('CHANNEL_ACCESS_TOKEN')
+BROADCAST_EP = "https://api.line.me/v2/bot/message/broadcast"
+DEFAULT_HEADER = {'Content-type': 'application/json', 'Authorization': f"Bearer {CAT}"}
+
+
+# database API
+def open_pg():
+    return psycopg2.connect(DSN)
+
+
+def get_question():
+    with open_pg() as conn:
+        with conn.cursor(cursor_factory=DictCursor) as cur:
+            cur.execute('select * from questions')
+            questions = cur.fetchall()
+    return dict(random.choice(questions))
+
+
+# LINE API
+def broadcast_message(text):
+    body = {'messages': [{'type': 'text', 'text': text}]}
+    req = requests.post(BROADCAST_EP, data=json.dumps(body, ensure_ascii=False).encode('utf-8'), headers=DEFAULT_HEADER)
+    return req
+
+
+@route('/question', method='GET')
+def question():
+    q = get_question()
+    res = broadcast_message(q)
+    if res.status_code == 200:
+        return 'OK'
+    else:
+        return res.text
 
 
 @route('/line-callback', method='POST')
