07b29a24ad03387f8c40707f0cd6d367ad4f6ea6
yatabis <39yatabis.rim8820xxx@gmail.com>
2019-06-13 11:29:15 +0900

    add: register new questions

--- main.py
+++ main.py
@@ -82,12 +82,34 @@ def check_answer(postback):
     qid, ans = [p.split('=')[1] for p in data.split('&')]
     record = get_description(qid)
     text = "正解です！\n" if ans == str(record['answer']) else "不正解です！\n"
-    text += f"・解説：\n{record['description']}"
+    text += f"【解説】\n{record['description']}"
     res = reply_text(text, token)
     if res.status_code == 200:
         return 'OK'
     else:
         return res.text
+
+
+def pre_registration(token):
+    form_id = os.environ.get('FORM_ID')
+    target = "%27%E3%83%95%E3%82%A9%E3%83%BC%E3%83%A0%E3%81%AE%E5%9B%9E%E7%AD%94%201%27!A1:E1000"
+    ep = f"https://asia-northeast1-sheetstowebapi.cloudfunctions.net/api?id={form_id}&range={target}"
+    req = requests.get(ep)
+    questions = req.json()
+    p_latest, c_latest = "", ""
+    text = "現在予備登録されている問題一覧："
+    for q in sorted(questions, key=lambda x: x['タイムスタンプ']):
+        part = q['パートを選択']
+        chapter = q['章番号（半角数字のみ）']
+        number = q['問題番号（半角数字のみ）']
+        if part != p_latest:
+            text += '\n' + part + '\n'
+            p_latest = part
+        if chapter != c_latest:
+            text += f"第{chapter}章\n"
+            c_latest = chapter
+        text += f"Q.{number} {q.get('問題文', '')[:5]}...\n"
+    return reply_text(text, token)
 
 
 def reply_question(token):
@@ -123,8 +145,13 @@ def line_callback():
         reply_token = event['replyToken']
         if event_type == 'postback':
             ret.append(check_answer(event))
-        elif event_type == 'message' and event['message']['type'] == 'text' and "問題" in event['message']['text']:
-            ret.append(reply_question(reply_token))
+        elif event_type == 'message' and event['message']['type'] == 'text':
+            if "問題" in event['message']['text']:
+                ret.append(reply_question(reply_token))
+            elif event['message']['text'] == "登録":
+                ret.append(reply_text(os.environ.get('FORM_URI'), reply_token))
+            elif event['message']['text'] == "確認":
+                ret.append(pre_registration(reply_token))
         else:
             ret.append('OK')
     return '\n'.join(ret)
