a391cfecb4bd1c6294849a6b736237cb1ec4b918
yatabis <39yatabis.rim8820xxx@gmail.com>
2019-06-15 18:42:04 +0900

    fix: tiny bug

--- main.py
+++ main.py
@@ -207,7 +207,7 @@ def make_answer_message(qid, ans, uid):
     judge = ans == str(q['answer'])
     stk = random.choice(OK_STICKER if judge else NG_STICKER)
     part, chapter, section, statement = get_name(q['part'], q['chapter'], q['section'])
-    text = f"{part}\n第{q['chapter']}章 {chapter}\n"
+    text = f"【{part}】\n第{q['chapter']}章 『{chapter}』\n"
     if section:
         text += f"{q['section']}. {section}\n"
     text += f"問{q['number']}-{q['variation']}\n"
