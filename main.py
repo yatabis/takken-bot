8cdeb2a7f0d0df92ede15f72dc67060bdba3fa54
yatabis <39yatabis.rim8820xxx@gmail.com>
2019-06-15 14:43:13 +0900

    add: daily report

--- main.py
+++ main.py
@@ -152,6 +152,14 @@ def make_answer_message(qid, ans, uid):
     return message, judge
 
 
+def make_report_message(text, report, uid):
+    message = {'messages': [
+        {'type': 'text', 'text': text},
+        {'type': 'text', 'text': report}
+    ], 'to': uid}
+    return message
+
+
 # callback
 def check_answer(postback):
     user_id = postback['source']['userId']
@@ -207,9 +215,49 @@ def reply_question(token):
         return res.text
 
 
+def daily_report():
+    with open_pg() as conn:
+        with conn.cursor(cursor_factory=DictCursor) as cur:
+            cur.execute('select * from users')
+            all_scores = cur.fetchall()
+    for score in all_scores:
+        uid = score['id']
+        text = f"お疲れ様でした。\n本日の{score['name']}さんのスコアです。\n"
+        report = []
+        t, f, n, judge = 0, 0, 0, ""
+        for h in range(7, 23 + 1):
+            s = score[str(h)]
+            if s is True:
+                judge = "○"
+                t += 1
+            elif s is False:
+                judge = "×"
+                f += 1
+            elif s is None:
+                judge = "-"
+                n += 1
+            report.append(f"{h:>2}時：{judge}")
+        text += f"　正解：{t}問\n不正解：{f}問\n未解答：{n}問\n"
+        if n > 8:
+            text += "継続は力なり、毎日コツコツ問題を解きましょう。"
+        elif t >= 17:
+            text += "その調子です！頑張っていきましょう。"
+        else:
+            text += "しっかりと復習をして定着させていきましょう。"
+        push_message(make_report_message(text, '\n'.join(report), uid))
+
+
 # routing
 @route('/question', method='POST')
 def question():
+    hour = datetime.now().hour
+    if hour == 0:
+        """daily_report()"""
+        pass
+    if hour == 0 or hour == 7:
+        reset_judge()
+    if not 7 <= hour <= 23:
+        return 'night'
     q = get_question()
     q_message = make_question_message(q)
     res = broadcast_message(q_message)
@@ -234,11 +282,6 @@ def list_preregistered():
 @route('/line-callback', method='POST')
 def line_callback():
     # if os.environ.get('MENTAINANCE', False)
-    hour = datetime.now().hour
-    if hour == 0:
-        reset_judge()
-    if not 7 <= hour <= 23:
-        return 'night'
     debug = os.environ.get('DEBUG', False)
     event_list = request.json['events']
     ret = []
